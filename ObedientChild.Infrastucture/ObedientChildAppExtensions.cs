d1e57b196406100d4778b4c7d9d8cb46dde459e8
slidenergy <slidenergy@gmail.com>
2024-07-23 23:00:16 +0300

    remove deprecate call of method

--- ObedientChild.Infrastucture/ObedientChildAppExtensions.cs
+++ ObedientChild.Infrastucture/ObedientChildAppExtensions.cs
@@ -5,24 +5,23 @@ using ObedientChild.Domain;
 
 namespace ObedientChild.Infrastructure
 {
-	public static class ObedientChildAppExtensions
-	{
-		public static IServiceCollection AddObedientChildInfrastructure(this IServiceCollection services, string connectionString)
-		{
-			services.AddEntityFrameworkNpgsql()
-							.AddDbContext<ApplicationDbContext>(options => options
-								.UseLazyLoadingProxies()
-                                .UseNpgsql(connectionString))
-							.BuildServiceProvider();
+    public static class ObedientChildAppExtensions
+    {
+        public static IServiceCollection AddObedientChildInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options
+                        .UseLazyLoadingProxies()
+                        .UseNpgsql(connectionString))
+                    .BuildServiceProvider();
 
-			services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
 
-			services.AddScoped<IRepository<ApplicationUser, string>, EfRepository<ApplicationUser, string>>();
-			services.AddScoped<IAuthTokensRepository, EfAuthTokensRepository>();
+            services.AddScoped<IRepository<ApplicationUser, string>, EfRepository<ApplicationUser, string>>();
+            services.AddScoped<IAuthTokensRepository, EfAuthTokensRepository>();
 
-			services.AddScoped<DataAccessLayer>();
+            services.AddScoped<DataAccessLayer>();
 
-			return services;
-		}
-	}
+            return services;
+        }
+    }
 }
